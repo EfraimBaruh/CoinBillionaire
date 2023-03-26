73646c22aea3eed25ff5208593d150f612a74b84
Efraim Baruh Bölükbaşı <96468990+EfraimBaruh@users.noreply.github.com>
2023-03-27 00:15:37 +0300

    map 01 edited.

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,13 +1,47 @@
-
-using System;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    private float speed = 10f;
+    [SerializeField] private float speed;
+
+    [Header("Movement Clamp")] 
+    [SerializeField]
+    private Vector2 min;
+    [SerializeField]
+    private Vector2 max;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        SetMinMax();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontal, vertical);
+        
+        _rigidbody.AddForce(direction / speed);
+        /*transform.position += (Vector3)direction / 10;*/
+    }
 
     private void Update()
     {
-        
+        Vector3 position = transform.position;
+        transform.position = new Vector3(Mathf.Clamp(position.x, min.x, max.x),
+            Mathf.Clamp(position.y, min.y, max.y), position.z);
+    }
+
+    private void SetMinMax()
+    {
+        float ratio = Screen.height / Screen.width;
+
+        min.x = (1.15f * Mathf.Pow(ratio, 2)) - 5.86f * ratio + 0.33f;
+        max.x = (-1.15f * Mathf.Pow(ratio, 2)) + 5.86f * ratio - 0.33f;
+
     }
 }
